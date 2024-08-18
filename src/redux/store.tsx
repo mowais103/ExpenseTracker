2005f7be2e0863438090e3fdc2bed3972fc7a6e5
Muhammad Owais <mohammadowais103@gmail.com>
2024-08-18 23:33:01 +0500

    add version to persist config

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -9,6 +9,7 @@ const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
   stateReconciler: autoMergeLevel2,
+  version: 1,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
